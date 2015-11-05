636650446c2799a7596c47c171d753107d1d7d10
duvanbarrera <duvan.barrera@mail.escuelaing.edu.co>
2015-11-05 12:47:09 -0500

    crear sirve

--- src/main/resources/static/js/app/controller/blogctrl.js
+++ src/main/resources/static/js/app/controller/blogctrl.js
@@ -28,7 +28,7 @@ app.controller("blogCtrl", function($scope,$log,$http) {
         $log.debug($scope.entry);
         $http({
             method  : 'POST',
-            url     : 'blog',
+            url     : 'http://10.2.67.81:8080/blog',
             data    : $scope.entry
         }).success(function(data) {
             console.log(data);
